34a0f6ad8d98672d01545d3c4840bf1cd27e0c4b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-04-28 13:50:23 +0200

    Update webhook

--- puppet-ghostbuster.sh
+++ puppet-ghostbuster.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+export REF=$1
+export REPO_NAME=$2
+export REPO_CLONE_URL=$3
+
 cd /var/lib/git
-test -d $2 || git clone $1
-cd $2
+test -d $REPO_NAME || git clone $REPO_CLONE_URL
+cd $REPO_NAME
 git fetch
-git checkout $3
+git checkout $(cut -d/ -f3 <<<"${REF}")
+
 puppet-ghostbuster -s http://puppetdb:8080
