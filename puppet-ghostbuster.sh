6b69b4e7ad8413c553075912a11c43b71cba69a3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-04-29 09:12:17 +0200

    Apply webhook on $BRANCHES only

--- puppet-ghostbuster.sh
+++ puppet-ghostbuster.sh
@@ -4,10 +4,13 @@ export REF=$1
 export REPO_NAME=$2
 export REPO_CLONE_URL=$3
 
-cd /var/lib/git
-test -d $REPO_NAME || git clone $REPO_CLONE_URL
-cd $REPO_NAME
-git fetch
-git checkout $(cut -d/ -f3 <<<"${REF}")
+branch=$(cut -d/ -f3 <<<"${REF}")
+if echo "${BRANCHES}" | grep -q $branch ; then
+  cd /var/lib/git
+  test -d $REPO_NAME || git clone $REPO_CLONE_URL
+  cd $REPO_NAME
+  git fetch
+  git checkout $branch
 
-puppet-ghostbuster -s http://puppetdb:8080
+  puppet-ghostbuster -s http://puppetdb:8080
+fi
