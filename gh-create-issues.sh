51c20ca28dedd8d9875f912c536ef69e1fc6c507
Léo Depriester <leo.depriester@camptocamp.com>
2017-10-13 10:26:42 +0200

    Fix pagination, add break and log messages

--- gh-create-issues.sh
+++ gh-create-issues.sh
@@ -2,7 +2,20 @@
 
 url="$API_BASE_URL/repos/$REPO_OWNER/$REPO_NAME/issues?token=$TOKEN"
 
-issues=$(curl -q "$url" | jq -r ".[].title" )
+echo "[*] Retrieving issues..."
+issues=""
+i=0
+while true; do
+  i=$((i + 1))
+  data=$(curl -s -q "$url&page=$i" | jq -r ".[].title" )
+  if [ -n "$data" ]; then
+    issues=$(echo -e "$issues\n$data")
+  else
+    break
+  fi
+done
+
+echo "[*] Looking for new issues..."
 
 while read line; do
   exists=0
@@ -11,10 +24,13 @@ while read line; do
   while read issue; do
     if [ "$issue" = "$title" ]; then
       exists=1
+      break
     fi
   done <<< "$issues"
 
   if [[ $exists -eq 0 ]]; then
-    curl -q -H 'Content-Type: application/json' "$url" -d "$line"
+    curl -s -q -H 'Content-Type: application/json' "$url" -d "$line"
   fi
 done < "${1:-/dev/stdin}"
+
+echo "[+] Operation successfully completed."
